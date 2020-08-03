11827d8721d57a632f80fcec465851a8b24db0af
takumi-shibata <241448ta@gmail.com>
2020-08-03 18:31:48 +0900

    1周目ボタンの非表示まで終了

--- script.js
+++ script.js
@@ -7,13 +7,28 @@ $(function() {
 
   // indexメソッド
   // ①インデント番号の取得: $('セレクタ').index($('取得したいインデント番号のセレクタ'));
-  // ②クリックされた要素のインデント番号の取得: clickメソッド中で、var 「$なし」変数名 = $('クリックで取得するセレクタ').index($(this));
+  // ②クリックされた要素のインデント番号の取得: clickメソッド中で、var $なし変数名 = $('クリックで取得するセレクタ').index($(this));
   $('.index-btn').click(function() {
     $('.active').removeClass('active');
-
     var clickedIndex = $('.index-btn').index($(this));
-    $('.slide-b').eq(clickedIndex).addClass('active');    
+    $('.slide-b').eq(clickedIndex).addClass('active'); 
+
+    // 以下数字のボタン(index-btn)を押した時の「次へ」「前へ」(change-btn)の表示・非表示の設定
+    // activeがついているスライド画像のインデント番号の取得
+    var slideIndex = $('.slide-b').index($('.active'));
+    
+    // CSSで非表示にしたボタンを表示させる設定(※全てのスライドでボタンが非表示になっているため。必要がないのは最初のスライドの「前へ」と最後のスライドの「次へ」のみ。以下で設定)
+    $('.change-btn').show();
+    
+    // 最初のスライドの「前へ」と、最後のスライドの「次へ」のボタンの非表示の設定
+    // ※等価演算子「===」と「==」は判別基準が異なる！「===」は比較する値がどちらも「同一」な場合に用いる。「==」は比較する値が「等しい」場合に用いる。
+    if(slideIndex == 0) {
+      $('.prev-btn').hide();
+    } else if (slideIndex == 3) {
+      $('.next-btn').hide();
+    }   
   });
+
 
   // prevメソッド: 対象となるセレクタを基準に同じ階層にある1つ前の要素を取得
   // nextメソッド: 対象となるセレクタを基準に同じ階層にある1つ後の要素を取得
@@ -26,8 +41,8 @@ $(function() {
     $displaySlide.removeClass('active');
    
     // 条件分岐で「次へ」を押した時と「前へ」を押した時の処理
-    // 「this」は「次へ」「前へ」のボタンの事を指している。複数ある内特定の動作を行った時に特定の動作を行う処理
-    // 「this」(次へ もしくは 前へ)の動作を行った際、「next-btn」というクラスのあるhtmlを基に「hasClass」でthisの動作がhtmlに合っている処理(true)なのか間違っている処理(false)なのかを判断
+    //「this」は「次へ」「前へ」のボタンの事を指している。複数ある内特定の動作を行った時に特定の動作を行う処理
+    //「this」(次へ もしくは 前へ)の動作を行った際、「next-btn」というクラスのあるhtmlを基に「hasClass」でthisの動作がhtmlに合っている処理(true)なのか間違っている処理(false)なのかを判断
     if($(this).hasClass('next-btn')) {
       // 次へのボタンを押し、htmlにnext-btnが入っていてtrueの場合、次の画像にactiveを追加
       // 前へのボタンを押し、htmlにnext-btnが入っていなくてfalseの場合はelseの処理、前の画像にactiveを追加
@@ -35,5 +50,21 @@ $(function() {
     } else {
       $displaySlide.prev().addClass('active');
     }
+
+    // 以下「次へ」「前へ」のボタン(change-btn)を押した時の表示・非表示の設定
+    // activeがついているスライド画像のインデント番号の取得
+    var slideIndex = $('.slide-b').index($('.active'));
+    
+    // CSSで非表示にしたボタンを表示させる設定(※全てのスライドでボタンが非表示になっているため。必要がないのは最初のスライドの「前へ」と最後のスライドの「次へ」のみ。以下で設定)
+    $('.change-btn').show();
+    
+    // 最初のスライドの「前へ」と、最後のスライドの「次へ」のボタンの非表示の設定
+    // ※等価演算子「===」と「==」は判別基準が異なる！「===」は比較する値がどちらも「同一」な場合に用いる。「==」は比較する値が「等しい」場合に用いる。
+    if(slideIndex == 0) {
+      $('.prev-btn').hide();
+    } else if (slideIndex == 3) {
+      $('.next-btn').hide();
+    }
+
   });
 });
