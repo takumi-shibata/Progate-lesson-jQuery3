4503f133e547e5750cdc77fa3838e9b9e7ffa119
takumi-shibata <241448ta@gmail.com>
2020-08-16 00:37:07 +0900

    ２周目indexメソッド終了(コード修正)

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 $(function() {
-  // eqメソッド: jQueryオブジェクトの中から、eqの引数の数字と同じインデント番号の要素を取得できる
-  // $('セレクタ').eq(インデント番号).メソッド
+  // eqメソッド: jQueryオブジェクトの中から、eqの引数の数字と同じインデックス番号の要素を取得できる
+  // $('セレクタ').eq(インデックス番号).メソッド
   $('#hide-btn').click(function() {
     $('.slide').eq(1).fadeOut();
   });
 
 
   // indexメソッド
-  // ①インデント番号の取得: $('セレクタ').index($('取得したいインデント番号のセレクタ'));
-  // ②クリックされた要素のインデント番号の取得: clickメソッド中で、var $なし変数名 = $('クリックで取得するセレクタ').index($(this));
+  // ①インデックス番号の取得: $('セレクタ').index($('取得したいインデックス番号のセレクタ'));
+  // ②クリックされた要素のインデックス番号の取得: clickメソッド中で、var $変数名 = $('クリックで取得するセレクタ').index($(this));
   $('.index-btn').click(function() {
     $('.active').removeClass('active');
+
+    // 変数名は次のeqメソッドの引数に使用するため「$」がない
     var clickedIndex = $('.index-btn').index($(this));
     $('.slide-b').eq(clickedIndex).addClass('active'); 
   });
